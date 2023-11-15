6f5e859e44a69ed69b9bbca039f9a1dbe3f66585
leandro cortez <neroxdcod@gmail.com>
2023-11-15 10:26:30 -0300

    cambio dirname por path.resolve

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require ("path")
+const app = express();
+app.use(express.static('public'));
+
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.resolve('./views/home.html'));
+});
+
+app.get("/login", (req,res)=> {
+    res.sendFile(path.resolve("./views/login.html"))
+})
+
+app.get("/register", (req,res)=> {
+    res.sendFile(path.resolve("./views/register.html"))
+})
+
